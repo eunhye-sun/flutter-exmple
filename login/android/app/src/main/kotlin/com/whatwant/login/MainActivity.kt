ee0c42a7e5a589dacd1690d386087c049147085e
이은혜 <leh941123@naver.com>
2022-10-18 21:59:07 +0900

    flutter login ui
    ui 연습중

---
+++ login/android/app/src/main/kotlin/com/whatwant/login/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.whatwant.login
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ login/lib/main.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: const LoginPages(),
+    );
+  }
+}
+
+class LoginPages extends StatelessWidget {
+  const LoginPages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Login",
+          style: TextStyle(color: Colors.white, fontSize: 24),
+        ),
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          iconSize: 30,
+          color: Colors.white,
+          onPressed: () {},
+        ),
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 30.0,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: '아이디를 입력해주세요.',
+                    labelStyle: TextStyle(fontSize: 14, color: Colors.black26),
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: '비밀번호를 입력해주세요.',
+                    labelStyle: TextStyle(fontSize: 14, color: Colors.black26),
+                  ),
+                  keyboardType: TextInputType.text,
+                  obscureText: true,
+                ),
+                SizedBox(
+                  height: 50.0,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 40.0,
+                  child: OutlinedButton(
+                    onPressed: () {
+                      print('OutlinedButton is clicked');
+                    },
+                    child: Text('로그인'),
+                    style: OutlinedButton.styleFrom(
+                      side: BorderSide(
+                        color: Colors.orange,
+                        width: 1.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
